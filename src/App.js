0eabbb5b624d394c9da6a01314046781557d90ff
prerits590 <prerits590@gmail.com>
2023-06-23 12:02:51 +0530

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 import './App.css';
+import { Box, Heading, FormControl, FormLabel, Button, ChakraProvider, Alert } from "@chakra-ui/react";
+import { Container } from '@chakra-ui/react'
+import { useState } from 'react';
+import { color } from 'framer-motion';
 
 function App() {
+  const[isLoading, setIsLoading]= useState(false);
+
+  function clickEvent(){
+    setIsLoading(true);
+
+    setTimeout(()=>{
+      setIsLoading(false);
+      alert('Log-in Successful!');
+    }, 2000);
+
+    
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ChakraProvider>
+    <Box bg="red.600" w="100%" h='100vh' p={4} display='flex' justifyContent='center' border='solid white'>
+      <Container my="auto" border='solid white' centerContent>
+        <Box color="white" pt='0.7rem'>
+          <Heading as="h1">Log In / Sign Up</Heading>
+        </Box>
+        
+        <Box py='1rem'>
+          <FormControl>
+            <FormLabel color='white'> Email Address </FormLabel>
+            <input type='email'/>
+
+          </FormControl>
+        </Box>
+
+        <Box>
+          <FormControl color='white'>
+            <FormLabel> Password </FormLabel>
+            <input style={{color:'black'}} type='password'/>
+
+          </FormControl>
+        </Box>
+
+        <Box py='1rem'>
+        <Button rightIcon={<ArrowForwardIcon/>} colorScheme='blue' onClick={clickEvent} isLoading={isLoading}>Submit</Button>
+        </Box>
+
+      </Container>
+    </Box>
+    </ChakraProvider>
+  
   );
 }
 
